f6a82097f855748912c9b370aae258573499e0c2
sassan404 <hasan.ism.404@gmail.com>
2025-02-21 07:03:55 +0100

    update the post request to return all pricing modules if doby is empty

--- app/controllers/subscription_controller.rb
+++ app/controllers/subscription_controller.rb
@@ -4,14 +4,20 @@ class SubscriptionController < Sinatra::Base
 
   post '/v1/prices' do
     content_type :json
-    request_body = request.body.read
-    begin
-      subscription = Subscription.create_from_json(request_body)
-      price = subscription.compute_prices.to_json
-    rescue Exception => e
-      halt 400, { error: "Error with subscription: #{e.message}" }.to_json
+    request_body = request.body.read if request.body
+    if request_body.nil? || request_body.empty?
+      ($pricing_modules.map do |pricing_module|
+        JSON.parse(pricing_module.to_json)
+      end).to_json
     else
-      price
+      begin
+        subscription = Subscription.create_from_json(request_body)
+        price = subscription.compute_prices.to_json
+      rescue Exception => e
+        halt 400, { error: "Error with subscription: #{e.message}" }.to_json
+      else
+        price
+      end
     end
   end
 end
